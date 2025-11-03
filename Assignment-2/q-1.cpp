a436beff0627921e4b7a736d70530d120d4808b7
chiraglamba27 <lchirag482@gmail.com>
2025-11-04 00:28:49 +0530

    Create q-1.cpp

---
+++ Assignment-2/q-1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key)
+            return mid;  
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1;  
+}
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter " << n << " elements in sorted order:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Enter the key to search: ";
+    cin >> key;
+
+    int index = binarySearch(arr, n, key);
+
+    if (index != -1)
+        cout << "Key found at index " << index << endl;
+    else
+        cout << "Key not found in the array." << endl;
+
+    return 0;
+}
